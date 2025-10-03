6a2d880e8416717a1ab87b32d40041f7dc497033
VeeverSW <veever233@outlook.com>
2025-10-04 01:24:31 +0800

    feat: try use safe safe way to copy (Save VV's Computer!)

--- JiaTools/GlobalUsings/ImGui.cs
+++ JiaTools/GlobalUsings/ImGui.cs
@@ -1,5 +1,4 @@
 ﻿global using Dalamud.Bindings.ImGui;
-global using Dalamud.Bindings.ImGuizmo;
 global using Dalamud.Bindings.ImPlot;
 global using OmenTools.ImGuiOm;
 global using Dalamud.Interface.Utility.Raii;
